d08faf1bbdf2238294637cec0f904f7f4113ea4f
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-05-29 12:31:52 +0100

    Created user dashboard, added most functionality

---
+++ lofi-frontend/lofi-frontend/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace lofi_frontend.Models
+{
+    public class Project
+{
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<TaskTimer> Timers { get; set; } = new List<TaskTimer>();
+        public int UserId { get; set; }
+    }
+}
