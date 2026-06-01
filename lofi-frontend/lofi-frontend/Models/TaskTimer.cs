69c06e08dc5255c181ad2fa6f875931936d03ac6
Dooques <samuelgdukes@gmail.com>
2026-06-01 14:05:41 +0100

    Create Timer Component and update homepage styling

---
+++ lofi-frontend/lofi-frontend/Models/TaskTimer.cs
@@ -0,0 +1,12 @@
+﻿namespace lofi_frontend.Models
+{
+    public class TaskTimer
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int Duration { get; set; } // Duration in seconds
+        public bool IsActive { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
