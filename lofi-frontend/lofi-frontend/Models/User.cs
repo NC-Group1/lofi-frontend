4862a66b4a4c49812b0d63c0cc0b6caf3878a80d
Sam Dukes <74371077+Dooques@users.noreply.github.com>
2026-06-01 11:50:07 +0100

    Signup page (#5)
    * Create Yotutube Component
    
    * Added some styling for Youtube video
    
    * updated video style
    
    * Created Signup page
    
    * Created Signup page functionality
    
    * Added password length, password hiding and email needing @ functionality
    
    * Changed Age to Date of Birth
    
    * Add validation to check user is at least 13 years old and some styling
    
    * Update warning messages
    
    ---------
    
    Co-authored-by: Yee Mon Aung <yeemonaung.isure@icloud.com>
    Co-authored-by: Marcus Paterson <marcus_paterson@outlook.com>

--- lofi-frontend/lofi-frontend/Models/User.cs
+++ lofi-frontend/lofi-frontend/Models/User.cs
@@ -6,21 +6,29 @@ namespace lofi_frontend.Models
     public class User
     {
         public int Id { get; set; }
-        [Required (ErrorMessage = "Please Enter Your Firstname")]
+        
+        [Required (ErrorMessage = "Please enter your first name")]
         public string FirstName { get; set; } = "";
-        [Required(ErrorMessage = "Please Enter Your Lastname")]
+        
+        [Required(ErrorMessage = "Please enter your last name")]
         public string LastName { get; set; } = "";
-        [Required(ErrorMessage = "Please Enter Your Email")]
+        
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
         public string Email { get; set; } = "";
 
-        [Required(ErrorMessage = "Please Enter A Username")]
+        [Required(ErrorMessage = "Please enter a username")]
         public string Username { get; set; } = "";
 
-        [Required(ErrorMessage = "Please Enter Your Password")]
+        [Required(ErrorMessage = "Please enter your password")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = "";
 
         [Required]
-        public int Age { get; set; } = 0;
+        [DataType(DataType.DateTime)]
+        public DateTime DOB { get; set; } = new DateTime(
+            DateTime.Now.Year - 20, DateTime.Now.Month, DateTime.Now.Day);
 
         [Required]
         public Gender Gender { get; set; } = Gender.PreferNotToSay;
@@ -29,9 +37,9 @@ namespace lofi_frontend.Models
 
     public class UserLogin
     {
-        [Required(ErrorMessage = "Please Enter Your Username")]
+        [Required(ErrorMessage = "Please enter your username")]
         public string Username { get; set; } = "";
-        [Required(ErrorMessage = "Please Enter Your Password")]
+        [Required(ErrorMessage = "Please enter your password")]
         public string Password { get; set; } = "";
 
     }
