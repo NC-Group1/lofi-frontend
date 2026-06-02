2ea4351021f5f721261851af0446b2bfe2dcf0a6
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-06-02 14:48:37 +0100

    Created components for user settings page

---
+++ lofi-frontend/lofi-frontend/Models/SupportMessage.cs
@@ -0,0 +1,20 @@
+﻿using lofi_frontend.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace lofi_frontend.Models
+{
+    public class SupportMessage
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        public CountryCode SelectedCountry { get; set; } = CountryCode.UK;
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
