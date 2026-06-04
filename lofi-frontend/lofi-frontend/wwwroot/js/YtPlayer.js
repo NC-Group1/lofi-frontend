b3981b46499aee48b0a66278f8fa99c0f72e2502
Dooques <samuelgdukes@gmail.com>
2026-06-04 16:58:22 +0100

    Begin working on LoadVideo button for each search result

--- lofi-frontend/lofi-frontend/wwwroot/js/YtPlayer.js
+++ lofi-frontend/lofi-frontend/wwwroot/js/YtPlayer.js
@@ -1,17 +1,24 @@
 ﻿let player;
+let isPlayerReady = false;
 
 window.initializeYouTubePlayer = (elementId, videoId) => {
+    isPlayerReady = false;
     player = new YT.Player(elementId, {
         height: '100%',
         width: '100%',
         videoId: videoId,
         playerVars: {
             'playsinline': 1,
-            'controls': 0, 
+            'controls': 0,
             'disablekb': 1,
             'fs': 0,
             'rel': 0,
             'mute': 0,
+        },
+        events {
+        'onReady': () => {
+                isPlayerReady = true;
+            }
         }
     });
 };
@@ -20,3 +27,13 @@ window.initializeYouTubePlayer = (elementId, videoId) => {
 window.playYouTubeVideo = () => { if (player) player.playVideo(); };
 window.stopYouTubeVideo = () => { if (player) player.pauseVideo(); };
 window.resetSeeker = () => { if (player) player.seekTo(0); };
+window.loadVideoById = (id) => {
+    if (player) {
+        if (isPlayerReady) {
+            player.loadVideoById(id, 0, "large")
+        } else {
+            console.warn("Player is not ready yet...")
+            setTimeout(() => window.loadVideoById(id), 100)
+        }
+    }
+}
