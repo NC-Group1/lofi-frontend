ddd4c5e35c411bf4070a84b5d492536d48f50b87
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-06-05 13:54:59 +0100

    still in progress

---
+++ lofi-frontend/lofi-frontend/wwwroot/js/Pomodoro.js
@@ -0,0 +1,18 @@
+﻿window.pomodoro = {
+    playButtonSound: () => {
+        const sound = new Audio('assets/button-press.mp3');
+        sound.currentTime = 0.5;
+        sound.play();
+        setTimeout(() => sound.pause(), 300);
+    },
+
+    playAlarmSound: () => {
+        const alarm = new Audio('assets/alarm.mp3');
+        alarm.currentTime = 0;
+        alarm.play();
+        setTimeout(() => {
+            alarm.pause();
+            alarm.currentTime = 0;
+        }, 5000);
+    }
+};
